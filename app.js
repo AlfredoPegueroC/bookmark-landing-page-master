04ff1714ec4758a288c4b62f154dec68d509efad
AlfredoPegueroC <Alfredocapellan20@gmail.com>
2026-06-07 22:00:41 -0400

    Implement mobile navigation functionality and enhance styles for better responsiveness

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const tabs = document.querySelectorAll('.features-tabs .tab');
 const contents = document.querySelectorAll('.feature');
 const questions = document.querySelectorAll('.question-item');
 const questionParagraphs = document.querySelectorAll('.question p');
+const hamburger = document.querySelector('.hamburger');
+const navItems = document.querySelector('.nav-items');
+const closeMenu = document.querySelector('.close-menu');
 
   tabs.forEach((tab, index) => {
   tab.addEventListener('click', () => {
@@ -27,3 +30,11 @@ questions.forEach((question, index) => {
   }
   )
 })
+
+hamburger.addEventListener('click', () => {
+  navItems.classList.toggle('active');
+});
+
+closeMenu.addEventListener('click', () => {
+  navItems.classList.remove('active');
+});
